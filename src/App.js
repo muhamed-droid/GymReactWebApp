7d86655b9431e53ad9bd6475f6833b8d6c39614d
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-03-17 20:04:16 +0100

    Application setup finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import './App.css'
+import { Route, Routes} from 'react-router-dom';
+import {Box} from '@mui/material';
+import ExcerciseDetail from './pages/ExcerciseDetail.js';
+import Home from './pages/Home.js';
+import Navbar from './components/Navbar.js'
+import Footer from './components/Footer.js'
 
 
 const App = () => {
     return (
-        <div>App</div>
+        <Box width="400px">
+            <Navbar/>
+                <Routes>
+                    <Route path = "/" element={<Home/>}/>
+                    <Route path = "/exercise/:id" element={<ExcerciseDetail/>}/>
+                </Routes>
+            <Footer />
+        </Box>
     )
 }
 

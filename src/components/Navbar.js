27a69d16a920ef093167d8ee31a1db352a825333
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-03-17 20:37:27 +0100

    Navbar is kind of finished

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,8 @@ const Navbar = () => {
         <Stack
         direction="row"
         justifyContent="space-around"
-        sx = {{ gap: {sm : '122px', xs: '40px'}, mt: {sm : '32px', xs: '20px'}}}
+        sx = {{ gap: {sm : '122px', xs: '40px'}, mt: {sm : '32px', xs: '20px'}, justifyContent: 'none'}}
+        px ="20px"
         >
             <Link to="/">
                 <img src={Logo} alt="logo" style={{width: '48', height: '48', margin:'0 20px'}}/>

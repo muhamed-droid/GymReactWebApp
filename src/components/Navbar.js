7b95b6fe83ff62e37e5f316670035d215bbf2907
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-03-17 20:27:04 +0100

    Navbar modified to look as it should

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
+import {Stack} from '@mui/material';
+import Logo from '../assets/images/Logo.png';
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <Stack>
+            <Link to="/">
+                <img src={Logo} alt="logo" style={{width: '48', height: '48', margin:'0 20px'}}/>
+            </Link>
+            <Stack>
+                <Link to="/" style={
+                    {textDecoration: 'none', color: '#3A1212',
+                    borderBottom: '3px solid #FF2625'}
+                }>Home </Link>
+                <a href="#excercises" style={
+                {textDecoration: 'none', color: '#3A1212'}
+                }> Exercises </a>
+            </Stack>
+        </Stack>
     );
 };
 

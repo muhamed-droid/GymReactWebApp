69771ea6a7fb049e2b250f07fb3ad4bc618bbb3d
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-03-20 15:08:29 +0100

    HeroBanner component now modified

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {Box, Stack, Typography} from '@mui/material'
 
 const HeroBanner = () => {
     return (
-        <div>
-            
-        </div>
+        <Box sx={{
+            mt: {lg: '212px', xs: '70px'},
+            ml: {sm: '50px'}
+        }}>
+            <Typography color = "#FF2625"
+            fontWeight="600" fontSize="26px">
+                Fitness Club
+            </Typography>
+        </Box>
     );
 };
 

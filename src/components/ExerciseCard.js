23fc0f9ad6aacfaa38ad830a9103c1b9c397e733
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-03-21 15:06:45 +0100

    Okay, looks good, but I do not have gifs

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import  {Link} from 'react-router-dom';
+import {Button, Stack, Typography} from '@mui/material';
 
-const ExerciseCard = () => {
+
+const ExerciseCard = ({exercise}) => {
     return (
-        <div>
-            ExerciseCard
-        </div>
+        <Link className = "exercise-card" to = { `/exercise/${exercise.id}`}>
+            <img src={exercise.gifURL} alt={exercise.name} />
+            <Stack>
+                <Button sx={{
+                    ml: '21px', color: '#fff', background: '#ffa9a9', fontSize: '14px',
+                    borderRadius: '20px', textTransform: 'capitalize'
+                }}>
+                    {exercise.bodyPart}
+                </Button>
+                <Button sx={{
+                    ml: '21px', color: '#fff', background: '#fcc757', fontSize: '14px',
+                    borderRadius: '20px', textTransform: 'capitalize'
+                }}>
+                    {exercise.target}
+                </Button>
+            </Stack>
+            <Typography ml = "21px" color= "#000" fontWeight="bold" mt="11px" pb="10px"
+            textTransform="capitalize" fontSize="22px">
+                {exercise.name}
+            </Typography>
+        </Link>
+
     );
 };
 

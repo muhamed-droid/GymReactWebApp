36dd6a63f9388d3ff9821ad99b62bfbe2392663d
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-03-23 10:59:27 +0100

    Some code changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import './App.css';
-import ExerciseDetail from './pages/ExerciseDetail';
+import ExerciseDetail from './pages/ExcerciseDetail.js';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';

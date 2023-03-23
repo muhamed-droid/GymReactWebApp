825671fd14f23cd14a5541d0c8a982cb46929871
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-03-23 10:54:04 +0100

    More code cleanup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react'
-import './App.css'
-import { Route, Routes} from 'react-router-dom';
-import {Box} from '@mui/material';
-import ExcerciseDetail from './pages/ExcerciseDetail.js';
-import Home from './pages/Home.js';
-import Navbar from './components/Navbar.js'
-import Footer from './components/Footer.js'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
 
+import './App.css';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
-const App = () => {
-    return (
-        //with sx we make sure this website is responsive on larger devices.
-        //with m="auto" we set margin to auto
-        <Box width="400px" sx = {{width: {xl: '1488'}}} m="auto">
-            <Navbar/>
-                <Routes>
-                    <Route path = "/" element={<Home/>}/>
-                    <Route path = "/exercise/:id" element={<ExcerciseDetail/>}/>
-                </Routes>
-            <Footer />
-        </Box>
-    )
-}
+const App = () => (
+    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/exercise/:id" element={<ExerciseDetail />} />
+        </Routes>
+        <Footer />
+    </Box>
+);
 
-export default App
+export default App;

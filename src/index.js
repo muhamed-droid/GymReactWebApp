462555188c42319bf77beea532ec0f7f6bae949a
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-04-03 12:43:32 +0200

    Cleaning some code

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+//Browser router keeps our UI in sync with URL.
+//It is the parent component used to store all other components.
+//React.StrictMode enables strict mode for the entire app, which help catch potential issues
+//and enforces best practices.
+
 root.render(
     <React.StrictMode>
         <BrowserRouter>
